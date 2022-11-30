a8dafa38d93687443f27527c6c1d7a7dd2342459
Mészöly Márton <meszoly.marton@gmail.com>
2022-11-30 22:08:23 +0100

    Filter by category

--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -2,17 +2,20 @@ import './MainContent.scss';
 import ItemGrid from '../ItemGrid/ItemGrid';
 import useFetch from '../../Hooks/useFetch';
 
-const MainContent = () => {
-    const { data, loading, error } = useFetch('https://fakestoreapi.com/products')
+const MainContent = props => {
+    let url = "https://fakestoreapi.com/products"
+    if(props.category !== "all"){
+        url = `${url}/category/${props.category}`
+    }
+    const { data, loading, error } = useFetch(url)
     const loadingItems = loading && <h2 className="loading">Loading...</h2>
     const errorMessage = (!data && error) && <h2 className="error">{error.message || 'Something went wrong!'}</h2>
-  
+
     return(
-        <>
-            {loadingItems}
+        <div className="main-content">
+            {loadingItems ? loadingItems : <ItemGrid items={data}/>}
             {errorMessage}
-            <ItemGrid items={data}/>
-        </>
+        </div>
     )
 }
 

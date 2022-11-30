4ed2c562d2588b32794d7af8698e269b49cc0921
Mészöly Márton <meszoly.marton@gmail.com>
2022-11-30 17:28:59 +0100

    Sidebar hotfix

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,17 +1,20 @@
 import './SideBar.scss';
 import useFetch from '../../Hooks/useFetch';
 import Categories from '../Categories/Categories';
+import Card from '../../UI/Card/Card';
 
 const SideBar = () => {
     const { data } = useFetch('https://fakestoreapi.com/products/categories')
-    console.log(data)  
-    
+    console.log(data)
+
     return (
         <div className="sidebar">
-            <h3 className="sidebar__title">Categories</h3>
-            <div className="sidebar__categories">
-                {data.map((item, index) => <Categories data={item} key={index}/>)}
-            </div>
+            <Card>
+                <h3 className="sidebar__title">Categories</h3>
+                <div className="sidebar__categories">
+                    {data.map((item, index) => <Categories data={item} key={index} />)}
+                </div>
+            </Card>
         </div>
     )
 }
